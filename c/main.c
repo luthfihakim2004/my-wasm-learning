637100f80f6d8e4a2f96875bbc226f936832b18a
luthfi.hakim <luthfihakim2004@mail.ugm.ac.id>
2025-04-21 15:17:38 +0700

    change to reading ELF file

--- c/main.c
+++ c/main.c
@@ -75,19 +75,3 @@ uint64_t get_entry_point(const uint8_t *bytes, int length) {
 
   return 0;
 }
-/*
- // Export the function using the EMSCRIPTEN_KEEPALIVE macro
- EMSCRIPTEN_KEEPALIVE
- void store_input(const char *input) {
-   if (saved_input) {
-     free(saved_input); // free old memory
-   }
-   saved_input = strdup(input); // allocate and copy input
-   printf("Stored input: %s\n", saved_input);
- }
-
- EMSCRIPTEN_KEEPALIVE
- const char *get_input() {
-   return saved_input ? saved_input : "(no input stored)";
-}
-*/
